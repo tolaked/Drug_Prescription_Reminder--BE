41ae699d341420732443657ef164dc05affd33e9
Akere Adetola <adetolaakere1@gmail.com>
2020-03-13 23:25:51 +0100

    user login

--- resources/users/user.validation.js
+++ resources/users/user.validation.js
@@ -24,4 +24,18 @@ module.exports = {
     };
     return Joi.validate(user, schema);
   },
+  validateLogin(user) {
+    const schema = {
+      email: Joi.string()
+        .min(2)
+        .max(50)
+        .required()
+        .email(),
+      password: Joi.string()
+        .min(5)
+        .max(50)
+        .required(),
+    };
+    return Joi.validate(user, schema);
+  },
 };
--- resources/users/userController.js
+++ resources/users/userController.js
@@ -16,7 +16,7 @@ const register = async (req, res) => {
 
 
     const { firstName, lastName, email, age, password } = req.body;
-    let doc = await Users.findOne({ email }, (err, user)=>{
+    let doc = await Users.findOne({ email }, (err, user) => {
       if (err) {
         return res.status(409).json({
           status: 409,
@@ -51,4 +51,39 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  try {
+    const { error } = validation.validateLogin(req.body);
+    if (error) {
+      return res.status(422).json({
+        status: 422,
+        error: error.details[0].message,
+      });
+    }
+    const { email, password } = req.body;
+
+    const existingUser = await Users.findOne({ email });
+    if (!existingUser) {
+      return res.status(400).json({
+        status: 400,
+        message: 'Invalid email or password',
+      });
+    }
+    const userPassword = await bcrypt.compareSync(password, existingUser.password);
+    if (!userPassword) {
+      return res.status(400).json({
+        message: 'invalid email or password',
+      });
+    }
+    return res.status(200).json({
+      message: 'Logged in successfully',
+      user: AuthHelper.Auth.toAuthJSON(existingUser),
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || 'Could not login user',
+    });
+  }
+};
+
+module.exports = { register, login };
