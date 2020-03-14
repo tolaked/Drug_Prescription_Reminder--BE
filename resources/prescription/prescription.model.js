8feaeeb0f58acc32dd136ed922779739dbb2e426
Akere Adetola <adetolaakere1@gmail.com>
2020-03-14 08:05:19 +0100

    prescription model

---
+++ resources/prescription/prescription.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const PrescriptionSchema = Schema({
+  drug: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+  },
+  unit: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+  },
+  start_Date: {
+    type: Date,
+  },
+  end_Date: {
+    type: Date,
+  },
+  userId: {
+    type: Number,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const PrescriptionModel = mongoose.model('User', PrescriptionSchema);
+module.exports = PrescriptionModel;
