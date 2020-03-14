942f49f04be2bec68067a33f5e419b5fad1f4fe0
Akere Adetola <adetolaakere1@gmail.com>
2020-03-14 12:10:22 +0100

    usage formula model

--- resources/usageFormula/usageFormula.model.js
+++ resources/usageFormula/usageFormula.model.js
@@ -1,36 +1,36 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
-const PrescriptionSchema = Schema({
-  drug: {
+const UsageFormaulaSchema = Schema({
+  frequency: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 50,
   },
-  unit: {
-    type: String,
+  number_of_times: {
+    type: Number,
     required: true,
-    minlength: 2,
+    minlength: 1,
     maxlength: 50,
   },
-  start_Date: {
-    type: Date,
+  dose: {
+    type: Number,
     required: true,
   },
-  end_Date: {
-    type: Date,
-    required: true,
-  },
-  userId: {
+  duration: {
     type: String,
     required: true,
   },
-  completed: {
+  prescription_id: {
+    type: String,
+    required: true,
+  },
+  before_after_meal: {
     type: Boolean,
     default: false,
   },
 });
 
-const PrescriptionModel = mongoose.model('Prescription', PrescriptionSchema);
-module.exports = PrescriptionModel;
+const UsageFormulaModel = mongoose.model('UsageFormula', UsageFormaulaSchema);
+module.exports = UsageFormulaModel;
