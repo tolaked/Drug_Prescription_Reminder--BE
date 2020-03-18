a604c30cf640a2e3c626f8660038b3bbc248f3e3
Akere Adetola <adetolaakere1@gmail.com>
2020-03-19 00:55:47 +0100

    get a specific user prescription by id

--- resources/prescription/prescription.routes.js
+++ resources/prescription/prescription.routes.js
@@ -6,12 +6,15 @@ const prescriptionController = require("./prescriptionController");
 const { verifyToken } = require('../../utils/validateToken');
 
 const { addPrescription, deletePrescription,
-  verifyCompletion, getAllPrescriptions } = prescriptionController;
+  verifyCompletion, getAllPrescriptions,
+  getSpecificPrescription } = prescriptionController;
 
 router.route("/");
 router.route("/add").post(verifyToken, addPrescription);
 router.delete('/:_id', verifyToken, deletePrescription);
 router.put('/:_id', verifyToken, verifyCompletion);
+router.get('/find/:_id', verifyToken, getSpecificPrescription);
 router.get('/', verifyToken, getAllPrescriptions);
+
 
 module.exports = router;
